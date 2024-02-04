745c9b7358b297275c12979341b1ef092423bcfa
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-02-04 23:39:21 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 
 # Remove existing local_manifests
 crave run --no-patch -- "rm -rf .repo/local_manifests && \
 # Remove these from previous runs
-rm -rf  frameworks/base packages/apps/Settings vendor/plros/prebuilt/apps/Cromite ; \
-cd frameworks/base && git rebase --abort && cd ../../packages/apps/Settings && git rebase --abort; \
+rm -rf  vendor/plros/prebuilt/apps/Cromite ; \
+cd frameworks/base ; git rebase --abort ; cd ../../packages/apps/Settings ; git rebase --abort; cd ../../.. \
 # Initialize repo with specified manifest
 repo init --depth 1 -u https://github.com/sounddrill31/plros_manifests.git -b lineage-20.0 --git-lfs && \
 # Clone local_manifests repository
