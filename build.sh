3b508ba13dbb2353c037685e655c6892d9b6176a
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-02-04 23:20:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,37 +12,27 @@ crave run --no-patch -- "rm -rf .repo/local_manifests && \
 # Remove these from previous runs
 rm -rf .repo/projects/frameworks/base.git .repo/projects/packages/apps/Settings.git frameworks/base packages/apps/Settings vendor/plros/prebuilt/apps/Cromite ; \
 # Initialize repo with specified manifest
-repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
-
-repo init --depth 1 -u https://github.com/sounddrill31/plros_manifests.git -b lineage-20.0 --git-lfs
-
+repo init --depth 1 -u https://github.com/sounddrill31/plros_manifests.git -b lineage-20.0 --git-lfs && \
 # Clone local_manifests repository
 git clone https://github.com/sounddrill31/local_manifests --depth 1 -b lineage-oxygen .repo/local_manifests && \
-
- # Sync the repositories
+# Sync the repositories
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
-
-# Clone Cromite app
-git clone https://gitlab.com/plros-lab/android_packages_apps_Cromite.git vendor/plros/prebuilt/apps/Cromite && \
-
 # Apply microG patch to Settings app
 wget -O packages/apps/Settings/microG.patch https://github.com/Divested-Mobile/DivestOS-Build/raw/master/Patches/LineageOS-20.0/android_packages_apps_Settings/0016-microG_Toggle.patch && \
 cd packages/apps/Settings  && \
 git am *.patch && \
 cd ../../.. && \
-
 # Apply microG patch to frameworks/base
 wget -O frameworks/base/microG.patch https://github.com/Divested-Mobile/DivestOS-Build/raw/master/Patches/LineageOS-20.0/android_frameworks_base/0036-Unprivileged_microG_Handling.patch && \
 cd frameworks/base && \
 git am *.patch && \
 cd ../.. && \
-
+# Clone Cromite app
+git clone https://gitlab.com/plros-lab/android_packages_apps_Cromite.git vendor/plros/prebuilt/apps/Cromite && \
 # Set up build environment
 source build/envsetup.sh && \
-
 # Lunch configuration
 lunch lineage_oxygen-userdebug && \
-
 # Build the ROM
 mka bacon && \
 echo "Date and time:" && \
