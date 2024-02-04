03d3178986359bf830c5c53bef1f319f1a65cdea
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-02-05 01:45:15 +0530

    Disable microG sig spoofing

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 
 # Remove existing local_manifests
 crave run --no-patch -- "rm -rf .repo/local_manifests && \
 # Remove these from previous runs
-rm -rf frameworks/base vendor/plros/prebuilt/apps/Cromite ; \
-cd packages/apps/Settings; git restore ; cd ../../.. \
+#rm -rf frameworks/base vendor/plros/prebuilt/apps/Cromite ; \
+#cd packages/apps/Settings; git restore ; cd ../../.. \
 # Initialize repo with specified manifest
 repo init --depth 1 -u https://github.com/sounddrill31/plros_manifests.git -b lineage-20.0 --git-lfs && \
 # Clone local_manifests repository
@@ -19,15 +19,15 @@ git clone https://github.com/sounddrill31/local_manifests --depth 1 -b lineage-o
 # Sync the repositories
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
 # Apply microG patch to Settings app
-wget -O packages/apps/Settings/microG.patch https://github.com/Divested-Mobile/DivestOS-Build/raw/master/Patches/LineageOS-20.0/android_packages_apps_Settings/0016-microG_Toggle.patch && \
-cd packages/apps/Settings  && \
-patch -p1 -i *.patch && \
-cd ../../.. && \
+#wget -O packages/apps/Settings/microG.patch https://github.com/Divested-Mobile/DivestOS-Build/raw/master/Patches/LineageOS-20.0/android_packages_apps_Settings/0016-microG_Toggle.patch && \
+#cd packages/apps/Settings  && \
+#patch -p1 -i *.patch && \
+#cd ../../.. && \
 # Apply microG patch to frameworks/base
-wget -O frameworks/base/microG.patch https://github.com/Divested-Mobile/DivestOS-Build/raw/master/Patches/LineageOS-20.0/android_frameworks_base/0036-Unprivileged_microG_Handling.patch && \
-cd frameworks/base && \
-patch -p1 -i *.patch && \
-cd ../.. && \
+#wget -O frameworks/base/microG.patch https://github.com/Divested-Mobile/DivestOS-Build/raw/master/Patches/LineageOS-20.0/android_frameworks_base/0036-Unprivileged_microG_Handling.patch && \
+#cd frameworks/base && \
+#patch -p1 -i *.patch && \
+#cd ../.. && \
 # Clone Cromite app
 git clone https://gitlab.com/plros-lab/android_packages_apps_Cromite.git vendor/plros/prebuilt/apps/Cromite && \
 # Set up build environment
