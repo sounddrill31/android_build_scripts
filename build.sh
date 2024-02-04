b9b8f1b975279b91ac2e5b789c50e9911357871c
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-02-04 23:05:13 +0530

    Remove folders under .repo too

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 
 # Remove existing local_manifests
 crave run --no-patch -- "rm -rf .repo/local_manifests && \
 # Remove these from previous runs
-rm -rf frameworks/base packages/apps/Settings vendor/plros/prebuilt/apps/Cromite ; \
+rm -rf .repo/projects/frameworks/base.git .repo/projects/packages/apps/Settings.git frameworks/base packages/apps/Settings vendor/plros/prebuilt/apps/Cromite ; \
 # Initialize repo with specified manifest
 repo init --depth 1 -u https://github.com/sounddrill31/plros_manifests.git -b lineage-20.0 --git-lfs
 
