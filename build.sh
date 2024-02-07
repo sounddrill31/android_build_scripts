ec744f74bf29c44f800491318361b8d815657c31
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-02-07 15:52:09 +0530

    remove system/libhidl

--- build.sh
+++ build.sh
@@ -14,6 +14,9 @@ repo init -u https://github.com/Evolution-X/manifest -b udc ;\
 # Clone local_manifests repository
 git clone https://github.com/Lafactorial/local_manifest --depth 1 -b Evo-14 .repo/local_manifests ;\
 
+# Removals
+rm -rf system/libhidl
+
 # Sync the repositories
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \ 
 
