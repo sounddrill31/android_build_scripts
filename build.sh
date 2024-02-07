570513b2658c23c6203a5507017e3300d055d5f9
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-02-07 23:54:46 +0530

    temp fix

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 
 
 # Run inside foss.crave.io devspace, in the project folder
 # Remove existing local_manifests
-crave run --no-patch -- "rm -rf .repo/local_manifests && \
+crave run --no-patch -- "rm -rf .repo/local_manifests hardware/qcom-caf/msm8953/audio && \
 # Initialize repo with specified manifest
 repo init --depth=1 -u https://github.com/LMODroid/manifest.git -b thirteen --git-lfs && \
 
