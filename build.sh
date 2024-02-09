fd4aa2cf72ba170ac0850a9b9490b913f0b0ba1f
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-02-09 15:49:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 
 crave run --no-patch -- "rm -rf .repo/manifests .repo/local_manifests hardware/qcom-caf/msm8953/audio && \
 
 # Print out Build URL
-echo "Build URL is: https://foss.crave.io/app/#/build/info/${DCJOBID}?team=14" ; \
+echo "Build URL is: https://foss.crave.io/app/\#/build/info/${DCJOBID}?team=14" ; \
 
 # Initialize repo with specified manifest
 repo init --depth=1 -u https://github.com/burhancodes/lmodroid.git -b thirteen --git-lfs && \
