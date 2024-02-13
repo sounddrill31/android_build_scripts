365f4089ead115ce2d4f0eeed8b4da891f8c468b
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-02-13 11:41:58 +0530

    Update build-clean.sh

--- build-clean.sh
+++ build-clean.sh
@@ -4,8 +4,8 @@ set -e
 #Credit to Meghthedev for the initial script 
 
 # Initialize repo with specified manifest
-repo init --depth 1 -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs
-
+ repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+ 
 # Run inside foss.crave.io devspace, in the project folder
 # Remove existing local_manifests
 crave run --clean  --no-patch -- "rm -rf .repo/local_manifests && \
